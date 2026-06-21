7e281c8bf4c45b2a8eb2bae74263a97f6f0b5354
Hugo Sacks <1712hugo@gmail.com>
2026-06-21 08:50:22 -0300

    Se corrige idioma de fechas a español

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { routes } from './app.routes';
+
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+
+    { provide: LOCALE_ID, useValue: 'es-AR' }
   ]
 };
+``
