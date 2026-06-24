38512d6eec77a78ade89233eac399f7cc9f82ef6
Hugo Sacks <1712hugo@gmail.com>
2026-06-24 09:44:19 -0300

    Se corrige fondeo de color principal

--- src/app/componentes/principal/principal.ts
+++ src/app/componentes/principal/principal.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
-  standalone: true,              // 👈 necesario
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './principal.html',
   styleUrls: ['./principal.scss'],
 })
-export class Principal {
-  hoy = new Date();              // 👈 ahora sí dentro de la clase
-}
+export class Principal implements OnInit {
 
+  hoy = new Date();
+  colorFondo: string = '';
+
+  ngOnInit() {
+
+    const dia = new Date().getDay();
+
+    const colores = [
+      'linear-gradient(135deg, #1e3a8a, #38bdf8)', // domingo
+      'linear-gradient(135deg, #0f172a, #1e293b)', // lunes
+      'linear-gradient(135deg, #1d4ed8, #9333ea)', // martes
+      'linear-gradient(135deg, #047857, #22c55e)', // miércoles
+      'linear-gradient(135deg, #b45309, #f59e0b)', // jueves
+      'linear-gradient(135deg, #7c3aed, #ec4899)', // viernes
+      'linear-gradient(135deg, #be123c, #fb7185)'  // sábado
+    ];
+
+    this.colorFondo = colores[dia];
+  }
+}
